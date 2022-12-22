25224d813f850d7d9a2c000875f25462fc8349a9
unknown <dag_98@hotmail.com>
2022-12-21 19:31:06 -0600

    delete comments in colors file

--- src/const/colors.js
+++ src/const/colors.js
@@ -1,28 +1,3 @@
-// BUTTONS
-
-// orangebutton = #FD7D39
-// bluebutton = #2D87A8
-// greenbutton = #84E052
-// whitebutton = #FFFFFF
-// bluebutton2 = #38B3E0
-// closebutton = #FB443B
-// addbuttondisabled = #B0B0B0
-// addbutonenabled = #38B3E0
-
-// TEXTS
-
-// NOPLAYERSTEXT = #979797
-// addplayer = #38B3E0
-// modaltitle = #3F454E
-// MULTIPLENAMES = #3F454E
-// ADDFOTO= #38B3E0
-// SUBTITLESMODAL = #979797
-// Allbuttons = #FFFFFF
-// VERbutton = #38B3E0
-// historytitle = #FFFFFF
-// historyinfo = #FFFFFF
-// historydate = #FFFFFF
-
 const colors = {
   buttons: {
     blue: '#2D87A8',
